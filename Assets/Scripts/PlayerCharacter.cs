c4ce90aada71d2bb9412fd8dc36e6f9a94a552b0
Olegsander78 <onkotov@gmail.com>
2022-02-02 17:00:54 +0500

    Добавил другой вариант управления

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -6,21 +6,45 @@ using UnityEngine.SceneManagement;
 public class PlayerCharacter : MonoBehaviour
 {    
     public float JumpForce;
+    public float JumpSpeed;
     public Rigidbody2D Rig;
+
+    [SerializeField] private bool _isGrounded;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        //if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        //{
+        //    Rig.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Rig.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            if (_isGrounded)
+            {
+                Rig.velocity += new Vector2(0f, JumpSpeed);
+            }
         }
-        //Debug.Log(IsGrounded());
+        
     }
     bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0f, -1f, 0f),
             Vector2.down, 0.2f);
         return hit.collider != CompareTag("Ground");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        float dot = Vector2.Dot(collision.contacts[0].normal, Vector2.up);
+        if (dot > 0.5f)
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
     }
 
     public void GameOver()
