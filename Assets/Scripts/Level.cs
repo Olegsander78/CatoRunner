10d6d3202de463f55be0e0ee32ec423888e3705b
Oleg Kotov <onkotov@gmail.com>
2022-05-23 01:21:31 +0500

    Начал делать многослойный паралакс

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -8,7 +8,6 @@ public class Level : MonoBehaviour
     public const float LENGHT_VISUAL_SEGMENT = 30.7f;
 
     [SerializeField] private List<LevelSegment> _levelSegments = new List<LevelSegment>();
-    [SerializeField] private List<VisualLevelSegment> _visualLevelSegments = new List<VisualLevelSegment>();
 
     [Header("Level Parameters:")]
     [SerializeField] private float _startSpeedLevel = 3f;
@@ -25,7 +24,8 @@ public class Level : MonoBehaviour
     public int NumberLevel { get => _numberLevel; set => _numberLevel = value; }
     public bool CompletedLevel { get => _completedLevel; set => _completedLevel = value; }
     public bool LockedLevel { get => _lockedLevel; set => _lockedLevel = value; }
-
+    
+    [Space(15)]
     [SerializeField] private Quest _quest;
 
     [SerializeField] private BGMusic.MusicType _bgMusicType;
@@ -34,27 +34,63 @@ public class Level : MonoBehaviour
     [SerializeField] private PlayerCharacter _playerCharacter;
     public PlayerCharacter PlayerCharacter => _playerCharacter;
 
-    [Header("Level Visual Parameters:")]
-    [SerializeField] private float _startSpeedVisualLevel = 3f;
-    [SerializeField] private float _speedVisualLevel;
-    [SerializeField] private float _minCoordVisualLevelSegX;
-    [SerializeField] private float _maxCoordVisualLevelSegX;
-    public float MaxCoordVisualLevelSegX { get => _maxCoordVisualLevelSegX; set => _maxCoordVisualLevelSegX = value; }
-    public float MinCoordVisualLevelSegX { get => _minCoordVisualLevelSegX; set => _minCoordVisualLevelSegX = value; }
-    public float StartSpeedVisualLevel => _startSpeedVisualLevel;
-    public float SpeedVisualLevel { get => _speedVisualLevel; set => _speedVisualLevel = value; }
+    //[Header("Level Visual Parameters:")]
+    //[SerializeField] private List<VisualLevelSegment> _visualTopLevelSegments = new List<VisualLevelSegment>();
+    //[SerializeField] private List<VisualLevelSegment> _visualMiddleLevelSegments = new List<VisualLevelSegment>();
+    //[SerializeField] private List<VisualLevelSegment> _visualBackLevelSegments = new List<VisualLevelSegment>();
+    //[Space(15)]
+    //[SerializeField] private float _startSpeedVisualLevel = 3f;
+    //[SerializeField] private float _speedVisualLevel;
 
-    
+    //[SerializeField] private float _minCoordVisualTopLevelSegX;
+    //[SerializeField] private float _maxCoordVisualTopLevelSegX;
+    //[SerializeField] private float _minCoordVisualMiddleLevelSegX;
+    //[SerializeField] private float _maxCoordVisualMiddleLevelSegX;
+    //[SerializeField] private float _minCoordVisualBackLevelSegX;
+    //[SerializeField] private float _maxCoordVisualBackLevelSegX;
+
+    //public float MaxCoordVisualLevelSegX { get => _maxCoordVisualLevelSegX; set => _maxCoordVisualLevelSegX = value; }
+    //public float MinCoordVisualLevelSegX { get => _minCoordVisualLevelSegX; set => _minCoordVisualLevelSegX = value; }
+    //public float StartSpeedVisualLevel => _startSpeedVisualLevel;
+    //public float SpeedVisualLevel { get => _speedVisualLevel; set => _speedVisualLevel = value; }
+    [System.Serializable]
+    public class VisualLevelsNote
+    {
+        public List<VisualLevelSegment> VisualLevelSegments;
+
+        public bool EnableVisualLevel;
+        public string NameVisualLevel;
+        public float StartSpeedVisualLevel;
+        public float SpeedVisualLevel;
+        public float MinCoordVisualLevelSegX;
+        public float MaxCoordVisualLevelSegX;
+        public float VelocityMultiplyVisualLevel;
+
+        public VisualLevelsNote(bool enableVisualLevel, string nameLevel, List<VisualLevelSegment> visualLevelSegments, float startSpeedVisualLevel, 
+            float speedVisualLevel, float minCoordVisualLevelSegX, float maxCoordVisualLevelSegX, float velocityMultiplyVisualLevel)
+        {
+            EnableVisualLevel = enableVisualLevel;
+            NameVisualLevel = nameLevel;
+            SpeedVisualLevel = speedVisualLevel;
+            MinCoordVisualLevelSegX = minCoordVisualLevelSegX;
+            MaxCoordVisualLevelSegX = maxCoordVisualLevelSegX;
+            VelocityMultiplyVisualLevel = velocityMultiplyVisualLevel;
+        }
+    }
+
+    [Header("Level Visual Parameters:")]
+    [SerializeField] private List<VisualLevelsNote> _visualLevelsNotes;
+
 
     private void Awake()
     {
         InitLevel();
-        InitVisualLevel();
+        //InitVisualLevels();
     }
     private void FixedUpdate()
     {
         MoveLevelSegment();
-        MoveVisualLevelSegment();
+        //MoveVisualLevelSegment();
         CheckFinishedQuest();
     }
 
@@ -77,22 +113,44 @@ public class Level : MonoBehaviour
         GameController.Instance.SoundController.PlayBGMusic(GameController.Instance.LevelController.CurrentLevel);
     }
 
-    private void InitVisualLevel()
+    //private void InitVisualLevel(List<VisualLevelSegment> visualLevelSegments, out float minCoordVisualLevelSegX, out float maxCoordVisualLevelSegX)
+    //{
+    //    SpeedVisualLevel = StartSpeedVisualLevel * visualLevelSegments[0].VelocityMultiply;
+
+    //    float minCoordVisualLevelSegX = visualLevelSegments[0].transform.position.x;
+    //    float maxCoordVisualLevelSegX = visualLevelSegments[0].transform.position.x;
+
+    //    for (int i = 1; i < visualLevelSegments.Count; i++)
+    //    {
+    //        if (minCoordVisualLevelSegX > visualLevelSegments[i].transform.position.x)
+    //        { minCoordVisualLevelSegX = visualLevelSegments[i].transform.position.x; }
+
+    //        if (maxCoordVisualLevelSegX < visualLevelSegments[i].transform.position.x)
+    //        { maxCoordVisualLevelSegX = visualLevelSegments[i].transform.position.x; }
+    //    }
+    //}
+    private void InitVisualLevels()
     {
-        SpeedVisualLevel = StartSpeedVisualLevel * _visualLevelSegments[0].VelocityMultiply;
-
-        MinCoordVisualLevelSegX = _visualLevelSegments[0].transform.position.x;
-        MaxCoordVisualLevelSegX = _visualLevelSegments[0].transform.position.x;
-
-        for (int i = 1; i < _visualLevelSegments.Count; i++)
+        foreach (var visualSegment in _visualLevelsNotes)
         {
-            if (MinCoordVisualLevelSegX > _visualLevelSegments[i].transform.position.x)
-            { MinCoordVisualLevelSegX = _visualLevelSegments[i].transform.position.x; }
+            visualSegment.SpeedVisualLevel = visualSegment.StartSpeedVisualLevel * visualSegment.VelocityMultiplyVisualLevel;
 
-            if (MaxCoordVisualLevelSegX < _visualLevelSegments[i].transform.position.x)
-            { MaxCoordVisualLevelSegX = _visualLevelSegments[i].transform.position.x; }
+            visualSegment.MinCoordVisualLevelSegX = visualSegment.VisualLevelSegments[0].Transform.position.x;
+            visualSegment.MaxCoordVisualLevelSegX = visualSegment.VisualLevelSegments[0].Transform.position.x;
+
+
         }
-    }    
+
+        //for (int i = 1; i < visualLevelSegments.Count; i++)
+        //{
+        //    if (minCoordVisualLevelSegX > visualLevelSegments[i].transform.position.x)
+        //    { minCoordVisualLevelSegX = visualLevelSegments[i].transform.position.x; }
+
+        //    if (maxCoordVisualLevelSegX < visualLevelSegments[i].transform.position.x)
+        //    { maxCoordVisualLevelSegX = visualLevelSegments[i].transform.position.x; }
+        //}
+    }
+
     private void MoveLevelSegment()
     {
         float displacement = SpeedLevel * Time.fixedDeltaTime;
@@ -117,27 +175,27 @@ public class Level : MonoBehaviour
         }
     }
 
-    private void MoveVisualLevelSegment()
-    {
-        float displacementVisual = SpeedVisualLevel * _visualLevelSegments[0].VelocityMultiply * Time.fixedDeltaTime;
+    //private void MoveVisualLevelSegment()
+    //{
+    //    float displacementVisual = SpeedVisualLevel * _visualLevelSegments[0].VelocityMultiply * Time.fixedDeltaTime;
 
-        foreach (var visualSegments in _visualLevelSegments)
-        {
-            Vector3 pos = visualSegments.transform.position;
-            pos.x -= displacementVisual;
+    //    foreach (var visualSegments in _visualLevelSegments)
+    //    {
+    //        Vector3 pos = visualSegments.transform.position;
+    //        pos.x -= displacementVisual;
 
-            if (pos.x < MinCoordVisualLevelSegX)
-            {
-                pos.x = MaxCoordVisualLevelSegX - (MinCoordVisualLevelSegX - pos.x) + LENGHT_VISUAL_SEGMENT;
-            }
-            visualSegments.Rigidbody.MovePosition(pos);
-        }
-    }
+    //        if (pos.x < MinCoordVisualLevelSegX)
+    //        {
+    //            pos.x = MaxCoordVisualLevelSegX - (MinCoordVisualLevelSegX - pos.x) + LENGHT_VISUAL_SEGMENT;
+    //        }
+    //        visualSegments.Rigidbody.MovePosition(pos);
+    //    }
+    //}
     
     public void ChangeSpeedLevel(float speedUp)
     {
         SpeedLevel *= speedUp;
-        SpeedVisualLevel *= speedUp * _visualLevelSegments[0].VelocityMultiply;
+        //SpeedVisualLevel *= speedUp * _visualLevelSegments[0].VelocityMultiply;
     } 
     
     private void CheckFinishedQuest()
